87b97b5b284f0c6f986c97174de79d612ea1fd8f
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-06-03 20:40:54 +0530

    My First Commit

---
+++ projbackend/routes/auth.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+const { check, validationResult } = require("express-validator");
+const { signout, signup, signin } = require("../controllers/auth");
+
+router.post(
+  "/signup",
+  [
+    check("name", "name should be at least 3 char").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char").isLength({ min: 3 })
+  ],
+  signup
+);
+
+router.post(
+  "/signin",
+  [
+    
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char").isLength({ min: 3 })
+  ],
+  signin
+);
+
+router.get("/signout", signout);
+
+module.exports = router;
