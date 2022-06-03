6e508f476ba9f386de78170209ecb1036bddb722
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-06-03 21:50:32 +0530

    added  Custom Middlewares

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -85,3 +85,21 @@ exports.isSignedIn = expressJwt({
 
 });
  
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+if(!checker){
+  return res.status(403).json({
+      error: "ACCESS DENIED"
+  })
+}
+next();
+}
+
+exports.isAdmin = (req, res, next) => {
+  if(req.profile.role === 0){
+    return res.status(403).json({
+      error: "You are not admit"
+    })
+  }
+  next();
+}
