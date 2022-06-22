3bba04bb7e57ba92bf007bfa7183b3ce7239bf2f
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-06-22 14:14:37 +0530

    section 9 starts

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -34,4 +35,50 @@ exports.updateUser = (req, res) => {
       res.json(user);
     }
   );
+};
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No Order in this account"
+        });
+      }
+      return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = ( req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach(product => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id
+
+    });
+  });
+
+  //store this in DB
+  User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $push: { purchases: purchases }},
+      { new: true },
+      (err, purchases) => {
+        if (err) {
+          return res.status(400).json({
+            error: "Unable to save purchase list"
+          });
+        }
+        next();
+      }
+      );
+
+
 };
