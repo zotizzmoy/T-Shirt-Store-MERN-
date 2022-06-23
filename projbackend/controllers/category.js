22dfc254253ed1a0c98bcf7cb1d099b27b3a5a84
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-06-23 14:58:20 +0530

    Section 9

---
+++ projbackend/controllers/category.js
@@ -0,0 +1,28 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+
+    Category.findById(id).exec((err, cate) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Category not found in DB"
+            });
+        }
+            req.category = cate;
+            next();
+
+        });``
+
+};
+
+exports.createCategory = (req, res) => {
+    const category = new Category(req.body);
+    category.save((err, category) =>{
+        if (err) {
+            return res.status(400).json({
+                error:"Not able to save category in dB"
+            })
+        }
+        res.json({ category });
+    });
+};
