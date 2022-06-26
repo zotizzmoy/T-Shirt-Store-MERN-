de17ec7b3924810b7407523fc75267ea4ce72eec
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-06-26 13:28:36 +0530

    backend done

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,11 +7,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-//My Routes
+//My routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 //DB Connection
 mongoose
@@ -33,7 +34,8 @@ app.use(cors());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
-app.use("/api", productRoutes );
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
@@ -42,3 +44,4 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
+
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct} = require("../controllers/product")
+const { getProductById, createProduct, getProduct, photo, updateProduct, deleteProduct} = require("../controllers/product")
 const { isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const { getUserById} = require("../controllers/user")
 
@@ -11,5 +11,27 @@ router.param("productId", getProductById);
 
 
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct )
+
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+//delete route 
+router.delete(
+    "/product/:productId/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    deleteProduct
+);
+
+//update route
+
+router.put(
+    "/product/:productId/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    updateProduct
+);
 
 module.exports = router;
