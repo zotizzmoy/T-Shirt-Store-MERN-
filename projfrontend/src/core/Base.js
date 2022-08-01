d7e9c99cb11357a926378d2a2f772021ec072d76
Zotizzmoy <zotizzmoydeka@gmail.com>
2022-08-01 10:12:20 +0530

    Base component

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Base = () => (
+    <div>
+       <div className="container-fluid">
+            <div className="jumbotron bg-dark text-white text-center">
+                <h2 className="display-4">My Title</h2>
+                <p className="Lead">My description</p>
+            </div>
+            <p>This is main content</p>
+       </div>
+       <footer className="footer bg-dark mt-auto py-3"></footer>
+    
+    </div>
+)
+ 
