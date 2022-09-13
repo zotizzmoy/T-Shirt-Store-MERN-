7a9d834ffc39f857954c73a0e9a33d4ed8587aa1
zotizzmoy <zotizzmoydeka@gmail.com>
2022-09-13 00:00:44 -0700

    error in signup

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,41 +1,92 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 
 const Signup = () => {
-    const signUpForm = () => {
-        return (
-            <div className="row">
-                <div className="col-md-6 offset-sm-3 text-left">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-light">Name</label>
-                            <input className="form-control" type="text" />
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
 
-                        </div>
-                        <div className="form-group">
-                            <label className="text-light">Email</label>
-                            <input className="form-control" type="text" />
+  const { name, email, password, error, success } = values;
 
-                        </div>
-                        <div className="form-group">
-                            <label className="text-light">Password</label>
-                            <input className="form-control" type="text" />
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
 
-                        </div>
-                        <button className="btn btn-success btn-block">Submit</button>
-                    </form>
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+        }
+      })
+      .catch(console.log("Error in signup"));
+  };
 
-                </div>
-            </div>
-        )
-    }
+  const signUpForm = () => {
     return (
-        <Base title="Sign up page" description="A signup page!">
-            
-            {signUpForm()}
-        </Base>
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+                value={email}
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     );
+  };
+
+  return (
+    <Base title="Sign up page" description="A page for user to sign up!">
+      {signUpForm()}
+      <p className="text-light text-center">{JSON.stringify(values)}</p>
+    </Base>
+  );
 };
 
 export default Signup;
